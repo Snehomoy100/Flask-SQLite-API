5583805c33bfc5e008bb7cb45b03b88d37104b77
snehomoy100@gmail.com <snehomoy100@gmail.com>
2021-12-09 13:42:40 +0530

    timestamp added for db

--- app.py
+++ app.py
@@ -6,14 +6,15 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
 
+
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(200), nullable=False)
     completed = db.Column(db.Integer, default=0)
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
-
     def __repr__(self):
         return '<Task %r>' % self.id
+
 
 @app.route('/')
 def index():
